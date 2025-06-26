b466733da15ff6ebed5010b2cff956b9ee6060d3
DALBONG <dalbong21229@naver.com>
2025-06-26 09:55:48 +0900

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    //프로그램 실행 : main메소드가 있어야 실행가능
+    public static void main(String[] args){ //JAVA를 돌리기 위한 메인 코드
+        System.out.printIn("Hello");
+        
+    }
+}
